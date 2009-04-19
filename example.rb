d1c1c44eb3e48a1901018959cae9335a0cc1583e
James Pozdena <jpoz@jpoz.net>
2009-04-19 14:12:50 -0700

    screwed around on the airpline... dont remember what I did.

--- example.rb
+++ example.rb
@@ -1,14 +1,20 @@
 #!/usr/local/bin/macruby
 require 'gingerbeer'
 
-@points = [120,30,300,250,100,300,20,100]
+@points = [10,7,9,10,10,5,6,6,3,6,7]
 
-canvas = Canvas.new :type => :image, :filename => 'line.png', :size => [900,500]
+canvas = Canvas.new :type => :image, :filename => 'line.png', :size => [2000,1000]
 canvas.background(Color.new(0.1,0.1,0.1))
 
-graph = LineGraph.new(100, 100, 700, 300)
+graph = LineGraph.new(100, 100, 1800, 350)
 graph.points = @points
-graph.title = "This is my Graph"
+graph.base_stroke = Color.new(0.2,0.6,0.4)
+graph.fill = Color.new(0.2,0.2,0.2)
+graph.vertical = false
+graph.numbers  = true
+graph.draw(canvas)
+
+canvas.translate(0,500)
 graph.draw(canvas)
 
 canvas.save
--- lib/line_graph.rb
+++ lib/line_graph.rb
@@ -1,5 +1,4 @@
 #!/usr/local/bin/macruby
-
 require 'hotcocoa/graphics'
 include HotCocoa
 include Graphics
@@ -9,17 +8,15 @@ class LineGraph
   attr_accessor :base_width, :back_width, :main_width
   attr_accessor :base_stroke, :back_stroke, :main_stroke
   attr_accessor :base_fill, :back_fill, :main_fill
-  attr_accessor :font, :font_size, :title
+  attr_accessor :scale, :base_line, :horizontal, :vertical, :numbers
   
   def initialize(x,y,width,height)
+    @numbers = @base_line = @horizontal = @vertical = @scale = true
     @points = nil
     @x, @y, @width, @height = x, y, width+0.0, height+0.0
     self.stroke = Color.new(1,1,1)
     self.fill = Color.new(0.9,0.9,0.9)
     self.strokewidth= 2
-    @font = 'Myraid Pro'
-    @font_size = 24
-    @title = nil
   end
   
   def stroke=(stroke)
@@ -35,10 +32,10 @@ class LineGraph
   end
   
   def draw(canvas)
-    draw_base(canvas)
-    draw_back(canvas)
+    draw_base(canvas) if base_line
+    draw_back(canvas) if horizontal
     draw_main(canvas)
-    add_title(canvas) if @title
+    draw_scale(canvas) if scale
   end
   
   private
@@ -57,7 +54,7 @@ class LineGraph
     diff = @width/(@points.size-1)
     x = @x
     back_points = @points.map do |p|
-      point = [x,@y+p]
+      point = [x,@y+(p*multi)]
       x += diff
       point
     end
@@ -65,24 +62,45 @@ class LineGraph
   end
   
   def draw_main(canvas)
+    font_size = 24
+    canvas.font('Myraid Pro')
+    canvas.fontsize(font_size)
+    
     canvas.stroke(@main_stroke)
     canvas.fill(@main_fill)
     canvas.strokewidth(@main_width)
     diff = (@width/(@points.size-1))
+    
     x = @x
-    # puts "diff #{diff} width #{@width} #{@points.size} points"
     @points.size.times do |i|
-      y = @y+@points[i]
+      y = @y+(@points[i]*multi)
       point = [x,y]
-      canvas.lines([[x,@y],point])
+      canvas.lines([[x,@y],point]) if vertical
       canvas.draw(Path.new.oval(x,y,10,10,:center))
+      canvas.text("#{points[i]}", x-(font_size/4), y+(font_size/2)) if numbers
       x += diff
     end
   end
   
-  def add_title(canvas)
+  def draw_scale(canvas)
+    # height = 100 #max = 32 #segments = 4 #segment_height = 25
+    font_size = 48
     canvas.font('Myraid Pro')
-    canvas.fontsize(48)
-    canvas.text(@title, @x, @y+@height-@font_size)
+    canvas.fontsize(font_size)
+    canvas.stroke(@back_stroke)
+    canvas.fill(@back_fill)
+    canvas.strokewidth(@back_width)
+    segments = ((@points.max/10.0).ceil+0.0)*2
+    segment_height = @height/segments
+    
+    (segments+1).times do |i|
+      y = @y+segment_height*i
+      canvas.text("#{i*5}", @x-(font_size/2)-20, y-(font_size/8))
+      canvas.lines([[@x-5,y],[@x-15,y]])
+    end
+  end
+  
+  def multi
+    @height/(((@points.max/10.0).ceil+0.0)*10)
   end
 end
