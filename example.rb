f39a8d4a085764374836a763e2077de2fb78ad69
James Pozdena <jpoz@jpoz.net>
2009-04-20 16:48:37 -0700

    changing around example

--- example.rb
+++ example.rb
@@ -1,20 +1,27 @@
 #!/usr/local/bin/macruby
 require 'gingerbeer'
 
-@points = [10,7,9,10,10,5,6,6,3,6,7]
+@points =  [10,7,9,100,10,5,6,6,3,6,7]
+@points2 = [5,6,1,2,4,100,9,9,3,3,10]
 
-canvas = Canvas.new :type => :image, :filename => 'line.png', :size => [2000,1000]
+canvas = Canvas.new :type => :image, :filename => 'line.png', :size => [900,500]
 canvas.background(Color.new(0.1,0.1,0.1))
 
-graph = LineGraph.new(100, 100, 1800, 350)
+graph = LineGraph.new(100, 100, 700, 300)
 graph.points = @points
-graph.base_stroke = Color.new(0.2,0.6,0.4)
+graph.base_stroke = Color.new(0.6,0.6,0.4)
 graph.fill = Color.new(0.2,0.2,0.2)
 graph.vertical = false
 graph.numbers  = true
 graph.draw(canvas)
 
-canvas.translate(0,500)
+graph2 = LineGraph.new(100, 100, 700, 300)
+graph.points = @points2
+graph.base_line = false
+graph.vertical  = false
+graph.scale     = false
+graph.stroke = Color.pink
+graph.fill   = Color.yellow
 graph.draw(canvas)
 
 canvas.save
